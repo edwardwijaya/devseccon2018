150bb9d8c9c714e2c60566e909a3676dd68ffc2e
Edward Wijaya <edward_wijaya@apple.com>
2018-02-23 14:04:07 +0800

    test

--- start_app_server.sh
+++ start_app_server.sh
@@ -2,7 +2,7 @@
 # README
 
 # READ SECRET!
-export MONGODB_CREDENTIALS=mongodb://devseccontest:devseccontest@ds147518.mlab.com:47518/devseccon-test
+#export MONGODB_CREDENTIALS=mongodb://devseccontest:devseccontest@ds147518.mlab.com:47518/devseccon-test
 
 # finally, run the app in dev mode...
 npm run dev
